bf423dcac4505b92e5f235f95c51bdc384980eb0
Daniel White <dm.white@gmx.com>
2023-05-09 00:59:51 +0100

    Added various shapes and levels

--- sketch.js
+++ sketch.js
@@ -5,7 +5,9 @@ var Engine = Matter.Engine,
 
 var engine;
 var box1;
-var ground;
+var ground1, ground2;
+var circle;
+var polygon;
 
 function setup() {
     createCanvas(900, 600);
@@ -13,29 +15,34 @@ function setup() {
     // Create an engine instance
     engine = Engine.create()
 
+    // Define shapes as Bodies instances
     box1 = Bodies.rectangle(200, 200, 80, 80, {restitution: .8, friction:0.5});
+    circle = Bodies.circle(80, 0, 20, {restitution: .6, friction:0.5});
+    polygon = Bodies.polygon(100, 0, 5, 30, {restitution: .9, friction:0.5});
 
-    var options = {isStatic: true, angle: Math.PI * 0.06}
-
-    ground = Bodies.rectangle(400, 500, 810, 10, options);
+    ground1 = Bodies.rectangle(100, 200, 500, 10, {isStatic: true, angle: Math.PI * 0.06});
+    ground2 = Bodies.rectangle(500, 500, 500, 10, {isStatic: true, angle: Math.PI * -0.06});
+    
     // Add of all the bodies to the world
-    World.add(engine.world, [box1, ground]);
-
+    World.add(engine.world, [box1, circle, polygon, ground1, ground2]);
 }
 
+// p5 draw() - called on each frame of the simulation
 function draw() {
     background(0);
     Engine.update(engine);
 
     fill(255);
     drawVertices(box1.vertices);
+    drawVertices(circle.vertices);
+    drawVertices(polygon.vertices);
 
     fill(125);
-    drawVertices(ground.vertices);
-
-
+    drawVertices(ground1.vertices);
+    drawVertices(ground2.vertices);
 }
 
+// Function to draw shapes to canvas
 function drawVertices(vertices) {
     beginShape();
     for (let i=0; i < vertices.length; i++) {
